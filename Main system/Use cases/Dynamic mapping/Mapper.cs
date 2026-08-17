dd1927a928e0b98a4dce69d172cde4e96ca34871
KAKACHI123EFZEF <125193387+KAKACHI123EFZEF@users.noreply.github.com>
2026-08-17 16:14:08 +0100

    Add mapping with generique type

--- Main system/Use cases/Dynamic mapping/Mapper.cs
+++ Main system/Use cases/Dynamic mapping/Mapper.cs
@@ -15,6 +15,26 @@
             }
         }
 
+
+        public static Type Map<T>(object source) where T : new()
+        {
+            var temp = typeof(T);
+
+            T ob = new();
+
+            foreach (var item in source.GetType().GetProperties())
+            {
+                var destininationPropety = temp.GetType().GetProperty(item.Name);
+                if (destininationPropety != null)
+                {
+                    var value = item.GetValue(source);
+                    destininationPropety.SetValue(ob, value);
+                }
+            }
+
+            return temp;
+        }
+
         public static void ToString(object obj)
         {
             foreach (var item in obj.GetType().GetProperties())
